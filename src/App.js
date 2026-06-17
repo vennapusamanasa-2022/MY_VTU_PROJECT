d790cbd7ac242829dfbe055c2a4ea6cb5a2636b7
vennapusamanasa-2022 <manasa.mrv2022@gmail.com>
2026-06-17 12:13:29 +0530

    created Footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
   <>
     <Header />
+    <Container>
+      <main>
+
+    </main>
+    </Container>
+    <Footer />
   </>
   );
 }
